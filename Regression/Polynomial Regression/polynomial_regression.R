51f40d12efc52b4849685dec2325bab64ec22373
Victor Hugo Miranda Pinto <victor_hmp@usp.br>
2018-02-06 13:54:26 -0200

    Add Polynomial Regression model
    Add implementations for polynomial regression model in both R and
    Python.
    
    Includes a sample dataset.

---
+++ Regression/Polynomial Regression/polynomial_regression.R
@@ -0,0 +1,46 @@
+# Polynomial Reression
+
+# Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Fitting the Linear Regression model to the dataset
+lin_reg = lm(formula = Salary ~ .,
+              data = dataset)
+
+# Fitting the Polynomial Regression model to the dataset
+dataset$Level2 = dataset$Level^2
+dataset$Level3 = dataset$Level^3
+dataset$Level4 = dataset$Level^4
+poly_reg = lm(formula = Salary ~ .,
+               data = dataset)
+
+# Visualising the Linear Regression results
+library(ggplot2)
+ggplot() + 
+  geom_point(aes(x = dataset$Level, y = dataset$Salary), 
+             color = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(lin_reg, newdata = dataset)), 
+            color = 'blue') +
+  ggtitle('Thruth or Bluff (Linear Regression)') +
+  xlab('Level') +
+  ylab('Salary')
+
+# Visualising the Polynomial Regression results
+ggplot() + 
+  geom_point(aes(x = dataset$Level, y = dataset$Salary), 
+             color = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(poly_reg, newdata = dataset)), 
+            color = 'blue') +
+  ggtitle('Thruth or Bluff (Polynomial Regression)') +
+  xlab('Level') +
+  ylab('Salary')
+
+# Predicting a new result with Linear Regression
+y_pred = predict(lin_reg, data.frame(Level = 6.5))
+
+# Predicting a new result with Polynomial Regression
+y_pred = predict(poly_reg, data.frame(Level = 6.5, 
+                                      Level2 = 6.5^2,
+                                      Level3 = 6.5^3,
+                                      Level4 = 6.5^4))
