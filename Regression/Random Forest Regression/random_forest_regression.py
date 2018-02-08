7e60b68e595915612734cab5c957a5837d76c21e
Victor Hugo Miranda Pinto <victor2142@gmail.com>
2018-02-08 19:26:05 -0200

    Add Random Forest Regression Model
    Add random forest regresison model implementations in Python and R.
    
    Includes sample dataset.

---
+++ Regression/Random Forest Regression/random_forest_regression.py
@@ -0,0 +1,39 @@
+# Random Forest Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+
+# Fitting the Random Forrest Regression Model to the dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 300, random_state = 0)
+regressor.fit(X, y)
+
+
+# Predicting a new result
+y_pred = regressor.predict(6.5)
+
+# Visualising the Random Forrest Regression results (for higher resolution)
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title("Thruth of Bluff (Random Forrest Regression Model)")
+plt.xlabel("Position level")
+plt.ylabel("Salary")
+plt.show()
